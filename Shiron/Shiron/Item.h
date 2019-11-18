f110ae4041233af2f7fa2be44a78ea0b65df2a38
Shira Dymnik <shirady@mta.ac.il>
2019-11-18 11:35:55 +0200

    new class - item

---
+++ Shiron/Shiron/Item.h
@@ -0,0 +1,42 @@
+#ifndef __ITEM_H
+#define __ITEM_H
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+#pragma warning(disable: 4996)
+
+class Item
+{
+public:
+	enum eCategory { KIDS, ELECTRONICS, OFFICE, CLOTHING }; //0,1,2,3
+	static const int MAX_LEN_NAME = 16;
+	static const int MIN_PRICE = 0; //free
+	static const int MAX_PRICE = 5000; //max price is up to 5,000 dollars
+	static const int MIN_SERIAL = 1;
+	static const int MAX_SERIAL = INT_MAX;
+
+private:
+	char* m_nameOfItem;
+	eCategory m_categoryOfItem;
+	int m_priceOfItem;
+	int m_serialNumberOfItem; // need to have a mechanism for a new serial number
+
+public:
+	bool setNameOfItem(const char* nameOfItem);
+	bool setCategoryOfItem(eCategory categoryOfItem);
+	bool setPriceOfItem(int priceOfItem);
+	bool setSerialNumberOfItem(int serialNumberOfItem);
+
+	const char* getNameOfItem();
+	eCategory getCategoryOfItem();
+	int getPriceOfItem();
+	int setSerialNumberOfItem();
+
+	Item(const char* nameOfItem, eCategory categoryOfItem, int priceOfItem, int serialNumberOfItem); //c'tor
+	Item(const Item& other); // copy c'tor
+	Item(Item&& other); // move c'tor
+	~Item(); //d'tor
+};
+
+#endif //__ITEM_H
